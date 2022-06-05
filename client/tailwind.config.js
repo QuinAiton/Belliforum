e3ff3a418857d21f3a5b238a691d436d000fcd0a
QuinAiton <quinaiton@hotmail.com>
2022-06-04 21:28:23 -0700

    Updates config and packages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,41 @@
 module.exports = {
-    content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideMenu: {
+          '0%': {
+            transform: 'translatex(0)'
+          },
+          '50%': {
+            transform: 'translatex(25vw)'
+          },
+          '100%': {
+            transform: 'translatex(50vw)'
+          }
+
+        },
+      },
+      animation: {
+        slideMenu: 'slideMenue 3s ease-in-out infinite',
+      },
+      colors: {
+        primary: '#015450',
+        secondary: '#ffffff',
+        tertiary: '##a0138e',
+
+      },
+      fontFamily: {
+        exo: ['Exo', 'sans-serif']
+      },
+      backgroundImage: {
+        'hero-image': "url('/assets/headerImage.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
   },
   plugins: [],
-}
+};
