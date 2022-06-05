6556ba8ee0b95189cbabdbc9ec3e828f500845de
QuinAiton <quinaiton@hotmail.com>
2022-06-04 21:26:26 -0700

    Adds Navigation

---
+++ client/components/Sidebar.js
@@ -0,0 +1,66 @@
+
+import { useState } from "react";
+
+const Sidebar = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+
+    return (
+        <nav id="nav" className="z-30 fixed bg-secondary w-screen h-[5vh]" >
+            <button onClick={() => setShowSidebar(!showSidebar)} className={`z-50 absolute top-0 right-0 hamburger hamburger--spring${showSidebar ? " is-active" : ""}`} type="button">
+                <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                </span>
+            </button>
+
+            {/* Large screen nav */}
+
+
+            {/*slider for mobile screens */}
+            <div
+                className={` w-3/4 md:w-1/2 top-0 right-0 bg-primary p-10 pl-10 text-white fixed h-full z-40  ease-in-out duration-300  ${showSidebar ? "translate-x-0 " : "translate-x-full"
+                    }`}
+            >
+                <ul className=" w-full flex flex-col items-start gap-6 text-sm">
+                    <li>
+                        <a className="text-secondary transition hover:text-secondary/75 text-xl" href="/">
+                            About
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="text-secondary transition hover:text-secondary/75 text-xl" href="/">
+                            Careers
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="text-secondary transition hover:text-secondary/75 text-xl" href="/">
+                            History
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="text-secondary transition hover:text-secondary/75 text-xl" href="/">
+                            Services
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="text-secondary transition hover:text-secondary/75 text-xl" href="/">
+                            Projects
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="text-secondary transition hover:text-secondary/75 text-xl" href="/">
+                            Blog
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Sidebar;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link href="dist/hamburgers.css" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
