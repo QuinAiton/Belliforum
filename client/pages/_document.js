c87f8b8c5776916ecade5ba46f7c9830831646d4
QuinAiton <quinaiton@hotmail.com>
2022-06-06 23:11:21 -0700

    Adds scroll to html

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
-        <Html>
+        <Html className='smooth-scroll'>
             <Head>
                 <link href="dist/hamburgers.css" rel="stylesheet"></link>
                 <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css' rel='stylesheet' />
