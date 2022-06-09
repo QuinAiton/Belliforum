4639e8dd9c41bc4dadb1b2a2a547a276525420fb
QuinAiton <quinaiton@hotmail.com>
2022-06-08 22:47:20 -0700

    Escapes appostophes

--- client/components/Home.js
+++ client/components/Home.js
@@ -42,7 +42,7 @@ const Home = () => {
                     <hr data-aos='fade-right' className='w-[30vw] border border-primary mt-2' />
                 </div>
                 <p data-aos='fade-left' className=' p-10 md:px-[20vw] text-center text-md md:text-xl'>
-                    Brand Loyalty must be earned and re-earned every day.Like fashion, what`'s in style and hot today, may change tomorrow.  Belliforum operates with a combined 100+ years of financial industry experience knowing that today`''s best may not always be.  We are not tied to any brand, institution, product, service, or firm.  We are here to find the best of the best and adapt immediatly.</p>
+                    Brand Loyalty must be earned and re-earned every day.Like fashion, what&aposs in style and hot today, may change tomorrow.Belliforum operates with a combined 100+ years of financial industry experience knowing that today&aposs best may not always be.  We are not tied to any brand, institution, product, service, or firm.  We are here to find the best of the best and adapt immediatly.</p>
             </div>
             <section
                 className="z-10 relative flex items-center justify-center h-screen bg-fixed bg-center bg-cover bg-yacht-image"
@@ -50,7 +50,7 @@ const Home = () => {
                 <div className="z-20 relative w-full h-screen bg-black bg-opacity-[55%] flex flex-col gap-5b justify-center items-center p-5 text-white">
                     <h1 data-aos='fade-right' className='text-4xl md:text-4xl'>Why Belliforum?</h1>
                     <hr data-aos='fade-left' className='w-[30vw] border border-secondary mt-2 mb-5' />
-                    <p data-aos='fade-right' className='p-10 md:px-[20vw] text-center text-md md:text-xl'>Because we`'re all about amplifying fun!  In plain English, we help you achieve what you want to do more rapidly and efficently!  Whatever that vision is, we want to be a part of it with you and have fun together doing it.  Our goal is to make your life better, to have more fun and do more of whatever it is that makes you want to get up in the morning.</p>
+                    <p data-aos='fade-right' className='p-10 md:px-[20vw] text-center text-md md:text-xl'>Because we&aposre all about amplifying fun!  In plain English, we help you achieve what you want to do more rapidly and efficently!  Whatever that vision is, we want to be a part of it with you and have fun together doing it.  Our goal is to make your life better, to have more fun and do more of whatever it is that makes you want to get up in the morning.</p>
                 </div>
             </section>
             <section className='grid md:grid-cols-2'>
