64fb80855d4a7558476e8e520f001d62d1a03d73
QuinAiton <quinaiton@hotmail.com>
2022-06-12 09:43:15 -0700

    Adds project sanity schemas

---
+++ server/schemas/Team.js
@@ -0,0 +1,25 @@
+export default {
+  name: "team",
+  title: "Team",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: "Name of team member?",
+    },
+    {
+      name: "title",
+      title: "Job Title",
+      type: "string",
+      description: "Where is there position at the company?",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+      description: "What would you like people to know about you?",
+    },
+  ],
+};
