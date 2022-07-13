9f8f911b6e3309cf4390a1e29ee80c072e25032c
QuinAiton <quinaiton@hotmail.com>
2022-07-12 22:36:17 -0700

    Updates head tag

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,15 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
-    return (
-        <Html >
-            <Head>
-                <link href="dist/hamburgers.css" rel="stylesheet"></link>
-                <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css' rel='stylesheet' />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
-};
+  return (
+    <Html>
+      <Head>
+        <title> Accelerate your vision with Belliforum</title>
+        <meta
+          name="description"
+          content="Belliforum connects successful entrepreneurs with the country’s best strategic partners in tax planning, business succession planning, partnerships, philanthropy, and private equity. We work to compliment your current team and optimize your goals to accelerate your vision."
+        ></meta>
+        <meta name="robots" content="index, about"></meta>
+        <link href="dist/hamburgers.css" rel="stylesheet"></link>
+        <link
+          href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
