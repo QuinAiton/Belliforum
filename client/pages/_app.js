56191226bb2eae208953d85f94557a14938ff0f0
QuinAiton <quinaiton@hotmail.com>
2022-07-25 07:45:51 -0700

    moves meta and title tags out of document head

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,27 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    AOS.init({
-    });
+    AOS.init({});
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title> Accelerate your vision with Belliforum</title>
+        <meta
+          name="description"
+          content="Belliforum connects successful entrepreneurs with the country’s best strategic partners in tax planning, business succession planning, partnerships, philanthropy, and private equity. We work to compliment your current team and optimize your goals to accelerate your vision."
+        ></meta>
+        <meta name="robots" content="index, about"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- client/pages/_document.js
+++ client/pages/_document.js
@@ -3,13 +3,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title> Accelerate your vision with Belliforum</title>
-        <meta
-          name="description"
-          content="Belliforum connects successful entrepreneurs with the country’s best strategic partners in tax planning, business succession planning, partnerships, philanthropy, and private equity. We work to compliment your current team and optimize your goals to accelerate your vision."
-        ></meta>
-        <meta name="robots" content="index, about"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <link href="dist/hamburgers.css" rel="stylesheet"></link>
         <link
           href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css"
