b70982ae522c89b9d4b941c158c628f7928acb67
QuinAiton <quinaiton@hotmail.com>
2022-08-10 21:10:53 -0700

    Takes padding off footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = ({ isRelative, disclaimer }) => {
         <div className={`z-50 h-auto  w-screen bg-primary py-2 ${isRelative ? "relative top-[90vh] md:top-[100vh] " : "static"}`}>
             <h4 className='text-white text-center mb-5'>Follow Us</h4>
             <SocialButtons /> 
-            <p className='text-center text-[9px] px-[10vw] md:px-[15vw] mx-auto pt-5 text-white'>{disclaimer}</p>
+            <p className='text-center text-[9px] px-[3vw] md:px-[15vw] mx-auto pt-5 text-white'>{disclaimer}</p>
         </div>
     );
 };
